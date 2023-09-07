ac45f2cf6f6471a0c1d8ca1d2b18247166f8fa55
Patrick Roughan <patrick@calypsoai.com>
2023-09-07 11:37:17 +0100

    new vagrant box

---
+++ platform-eng/scripts/install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# remove comment if you want to enable debugging
+#set -x
+
+if [ -e /etc/redhat-release ] ; then
+  REDHAT_BASED=true
+fi
+
+TERRAFORM_VERSION="1.5.6"
+PACKER_VERSION="1.9.4"
+# create new ssh key
+[[ ! -f /home/vagrant/.ssh/mykey ]] \
+&& mkdir -p /home/vagrant/.ssh \
+&& ssh-keygen -f /home/vagrant/.ssh/mykey -N '' \
+&& chown -R vagrant:vagrant /home/vagrant/.ssh
+
+# install packages
+if [ ${REDHAT_BASED} ] ; then
+  yum -y update
+  yum install -y docker ansible unzip wget
+else 
+  apt-get update
+  apt-get -y install docker.io ansible unzip python3-pip
+fi
+# add docker privileges
+usermod -G docker vagrant
+# install awscli and ebcli
+pip3 install -U awscli --break-system-packages
+pip3 install -U awsebcli --break-system-packages
+
+#terraform
+T_VERSION=$(/usr/local/bin/terraform -v | head -1 | cut -d ' ' -f 2 | tail -c +2)
+T_RETVAL=${PIPESTATUS[0]}
+
+[[ $T_VERSION != $TERRAFORM_VERSION ]] || [[ $T_RETVAL != 0 ]] \
+&& wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# packer
+P_VERSION=$(/usr/local/bin/packer -v)
+P_RETVAL=$?
+
+[[ $P_VERSION != $PACKER_VERSION ]] || [[ $P_RETVAL != 1 ]] \
+&& wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
+&& unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm packer_${PACKER_VERSION}_linux_amd64.zip
+
+# clean up
+if [ ! ${REDHAT_BASED} ] ; then
+  apt-get clean
+fi
